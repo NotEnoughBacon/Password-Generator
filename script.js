7da20cfac73a046542b546dc25b6c6e391761dd8
Julian Kenyon <jboyk3@hotmail.com>
2022-12-09 23:58:15 -0600

    added math logic, and printing out result

--- script.js
+++ script.js
@@ -5,35 +5,40 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-var result = ''
 var passwordChar = []
 // Write password to the #password input
 
 generateBtn.onclick = function getUserData () {
 
-  passwordLength = prompt ("How long would you like your password? \n Must be between 8-128 characters long.")
-  
-    var length = parseInt(passwordLength);
+  var result = ''
+
+  var passwordLength = prompt ("How long would you like your password? \n Must be between 8-128 characters long.")
   
     if (passwordLength == null) {
       alert ("Goodbye!")
       return;
     }
 
+    passwordLength = parseInt(passwordLength);
+
     if (passwordLength < 8 || passwordLength > 128) {
       alert ("Please choose a number between 8 and 128!")
       return getUserData();
-      }
+    }
 
     if (isNaN(passwordLength)) {
       alert ("Not a number, Please enter a valid number")
       return getUserData();
-      }
+    }
     
     else {
+
   lowerCaseConfirm = confirm ("Use lower case letters? \n OK for yes, cancel for NO")
+
   upperCaseConfirm = confirm ("Use upper case letters? \n OK for yes, cancel for NO")
+
   numericConfirm = confirm ("Use numbers? \n OK for yes, cancel for NO") 
+
   specialCharConfirm = confirm ("Use special characters? \n OK for yes, cancel for NO")
       
     if (!lowerCaseConfirm && !upperCaseConfirm && !numericConfirm && !specialCharConfirm) {
@@ -57,28 +62,14 @@ generateBtn.onclick = function getUserData () {
       passwordChar = passwordChar.concat(specialChar)
     }
 
-    
+    for (var i = 0; i < passwordLength; i++) {
+      result = result + passwordChar[Math.floor(Math.random() * passwordChar.length)]
+    }
 
+    var password = result
 
+    var passwordText = document.querySelector('#password')
 
-
-
-
+    passwordText.value = password
   }
 }
-
-
-
-/*
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
-
