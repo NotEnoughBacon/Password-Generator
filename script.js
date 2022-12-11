a569ddb9c62aea16440daf72f1745e4d7d334aaa
NotEnoughBacon <jboyk3@hotmail.com>
2022-12-10 21:50:13 -0700

    added var for confirms

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 // Setting an empty array to combine all selected character types
 var passwordChar = []
 
+
 // Main function to gather user imput data
 generateBtn.onclick = function getUserData () {
 //resetting result if user wants to generate multiple passwords
@@ -41,13 +42,13 @@ generateBtn.onclick = function getUserData () {
     
     else {
       //using my confirms to store true/false info on what type of password they want
-  lowerCaseConfirm = confirm ("Use lower case letters? \n OK for yes, cancel for NO")
+  var lowerCaseConfirm = confirm ("Use lower case letters? \n OK for yes, cancel for NO")
 
-  upperCaseConfirm = confirm ("Use upper case letters? \n OK for yes, cancel for NO")
+  var upperCaseConfirm = confirm ("Use upper case letters? \n OK for yes, cancel for NO")
 
-  numericConfirm = confirm ("Use numbers? \n OK for yes, cancel for NO") 
+  var numericConfirm = confirm ("Use numbers? \n OK for yes, cancel for NO") 
 
-  specialCharConfirm = confirm ("Use special characters? \n OK for yes, cancel for NO")
+  var specialCharConfirm = confirm ("Use special characters? \n OK for yes, cancel for NO")
     //if they didnt select anything, tell them to reconsider life, but to try again
     if (!lowerCaseConfirm && !upperCaseConfirm && !numericConfirm && !specialCharConfirm) {
       alert ("You must choose atleast one character type \n Please try again!")
@@ -69,6 +70,7 @@ generateBtn.onclick = function getUserData () {
     if (specialCharConfirm === true) {
       passwordChar = passwordChar.concat(specialChar)
     }
+
     //using the password length selected, using a Math.random to generate a random password using the user entered parameters
     for (var i = 0; i < passwordLength; i++) {
       result = result + passwordChar[Math.floor(Math.random() * passwordChar.length)]
